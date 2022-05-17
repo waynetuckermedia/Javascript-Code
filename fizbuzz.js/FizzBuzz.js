f0e9f4f0c2ac29b19ba0bf1bcd76654898d4eb6c
Wayne <waynetuckermedia@gmail.com>
2022-05-17 08:48:28 -0400

    5/17/2022 - function as param

---
+++ fizbuzz.js/FizzBuzz.js
@@ -0,0 +1,17 @@
+// PREP
+
+// 1 to num, 3 fizz, 5 buzzz, 3&5 fizzbuzz
+
+// P: parameters - num, wholenumber, positive, 
+// R: return - console.log - 
+// E: 
+
+function fizzBuzz(num){
+
+    //loop
+    //conditionals % 3&5,  %3. %5
+    //console.log num or fizz, buzz, fizzBuzz
+}
+fizzBuzz(5) // 1,2, fizz, 4, buzz
+fizzBuzz(3) //1,2, fizz
+fizzBuzz(15) // 1,2, fizz, 4, fizz, 7, 8, fizz, buzz, 11, fizz, 13, 14, fizzBuzz
